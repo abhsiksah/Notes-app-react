f2d02c81c3618ec7fefe2a1163ae3e3abec15b80
abhsiksah <killuawpak@gmail.com>
2021-05-23 16:10:57 +0530

    Updates

---
+++ precache-manifest.09e935be2cbca2a6cc1d377e6b8fc87b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b080e604f3648ef5d137d77113787e99",
+    "url": "/Notes-app-react/index.html"
+  },
+  {
+    "revision": "9e95d077c5ee17ecdbc3",
+    "url": "/Notes-app-react/static/css/main.e35954a9.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/Notes-app-react/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Notes-app-react/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e95d077c5ee17ecdbc3",
+    "url": "/Notes-app-react/static/js/main.425512be.chunk.js"
+  },
+  {
+    "revision": "d72af3f58c052f488ffc",
+    "url": "/Notes-app-react/static/js/runtime-main.d1d00dd7.js"
+  }
+]);
